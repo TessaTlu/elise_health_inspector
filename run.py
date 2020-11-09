347721f636072e62c87e4713d50c34e484d6e775
TessaTlu <43862134+TessaTlu@users.noreply.github.com>
2020-11-09 11:25:41 +0300

    :\

--- run.py
+++ run.py
@@ -45,7 +45,6 @@ while True:
                 else:
                     print("I did not find any reason to be worry")
                     print("but if you have symptoms of pneumonia, you should consult your doctor")
-        
 
 
 
