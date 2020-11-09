e14122186ed9f704a6a5481ea5fd063f3e2e47d4
TessaTlu <43862134+TessaTlu@users.noreply.github.com>
2020-11-09 13:00:54 +0300

    added comments

--- model_build.py
+++ model_build.py
@@ -15,34 +15,35 @@ heart_data = pd.read_csv('heart.csv')
 y = heart_data.target
 features = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']
 X = heart_data[features]
-train_X, val_X, train_y, val_y = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=1)
-train_data=np.int16(np.array(np.around(train_X*10, decimals=0)))
+train_X, val_X, train_y, val_y = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=1) ### Делим данные на обучающие и тестовые
+train_data=np.int16(np.array(np.around(train_X*10, decimals=0))) ### Готовим наши данные к тому, чтобы они стали последовательностью единиц и нулей
 train_labels=np.int16(np.array(train_y))
 test_data=np.int16(np.array(np.around(val_X*10, decimals=0)))
 test_labels=np.int16(np.array(val_y))
-
+### Функцтя нормализации будет использована для того, чтобы подготовить данные к отправке в input первого слоя нейросети
 def normalize(sequences, dimension = 65536 ):
     results = np.zeros((len(sequences), dimension))
     for i, sequence in enumerate(sequences):
         results[i, sequence]=1
     return results
 
+### Сама нормализация
 x_train=normalize(train_data)
 x_test=normalize(test_data)
 y_train=np.asarray(train_labels).astype('float32')
 y_test=np.asarray(test_labels).astype('float32')
-
+### Введение модели
 model = Sequential()
 model.add(layers.Dense(32, activation = 'relu', input_shape = (65536, )))
 model.add(layers.Dense(16, activation = 'relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
 
-
+### Компиляция модели
 model.compile(optimizer='rmsprop',
              loss='binary_crossentropy',
              metrics=['acc'])
-
+### Этап обучения 
 history = model.fit(x_train,
                    y_train,
                    epochs=13,
--- pneumonia_predict.py
+++ pneumonia_predict.py
@@ -7,18 +7,25 @@ import cv2
 import os
 from tensorflow import keras
 def scan_result(file_name):
-    img_size=150
-    array=Image.open(file_name)
+    img_size=150  ### ширина и высота картинки 
+    array=Image.open(file_name) 
     array=np.array(array)
+    ### Делаем картинку черно-белой, если она цветная
     if(len(array.shape)==3):
-        grayImage = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
+        grayImage = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY) 
         array=grayImage
     person = np.array(array)
     img_arr = person
+
+    ### Загружаем модель
     model = keras.models.load_model("x_ray_model")
+
+    ### Меняем размер картинки
     resized_arr = cv2.resize(img_arr, (img_size, img_size)) 
     resized_arr=resized_arr/255
     resized_arr=resized_arr.reshape(-1, img_size, img_size, 1)
+
+    ### Строим предположение с помощью ранее обученной модели
     result = model.predict(resized_arr)
     result =  np.around(result, decimals=0)
     result = int(result)
